9695ea4dc8ca2c4b5b94afa7973aea7e850a98e7
LarissaUFMS <89803958+LarissaUFMS@users.noreply.github.com>
2023-03-10 16:09:20 -0400

    Update IntArray.cpp

--- IntArray.cpp
+++ IntArray.cpp
@@ -1,7 +1,23 @@
-#include IntArray.h
+#include "IntArray.h"
+#include <algorithm>
 
 void
 IntArray::resize()
 {
+    ++_capacity;
+    int* tempData = new int[_capacity];
+    std::copy(_data, _data + _size - 1, tempData);
+    delete[]_data;
+    _data = tempData;
 
+}
+
+int*
+IntArray::find(int value) const
+{
+    for(int i = 0; i <= _size; ++i){
+        if(*(_data+i) == value)
+            return _data + i;
+    }
+    return nullptr;
 }
