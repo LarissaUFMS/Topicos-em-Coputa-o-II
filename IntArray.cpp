35f806d9e1cebc894941fed3cb152961285dcddb
LarissaUFMS <89803958+LarissaUFMS@users.noreply.github.com>
2023-03-10 17:25:33 -0400

    Update IntArray.cpp

--- IntArray.cpp
+++ IntArray.cpp
@@ -1,6 +1,7 @@
 #include "IntArray.h"
 #include <algorithm>
 
+//métodos privados
 void
 IntArray::resize()
 {
@@ -20,4 +21,15 @@ IntArray::find(int value) const
             return _data + i;
     }
     return nullptr;
+}
+
+//métodos públicos
+
+bool
+IntArray::remove(int value)
+{
+    auto position = find(value);
+    if(position == nullptr)
+        return false;
+    
 }
